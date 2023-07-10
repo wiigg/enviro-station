5955fcdf8dddbf173fae4fce66846d882782cf66
wiigg <danny@wigg.uk>
2023-07-10 20:08:23 +0100

    Simplify transmitted values

--- device/az_transmitter.py
+++ device/az_transmitter.py
@@ -10,20 +10,7 @@ class AzTransmitter:
 
     async def send_to_azure(self, values):
         """Send data to Azure IoT Hub"""
-        print(values)
-        pm_values = dict(i for i in values.items() if i[0].startswith("P"))
-        temp_values = dict(i for i in values.items() if not i[0].startswith("P"))
-
-        pm_values_json = [
-            {"value_type": key, "value": val} for key, val in pm_values.items()
-        ]
-        temp_values_json = [
-            {"value_type": key, "value": val} for key, val in temp_values.items()
-        ]
-
-        # Combine pm_values_json and temp_values_json
-        data = pm_values_json + temp_values_json
-        json_data = json.dumps(data)
+        json_data = json.dumps(values)
 
         try:
             logging.info("Sending data to IoT Hub...")
